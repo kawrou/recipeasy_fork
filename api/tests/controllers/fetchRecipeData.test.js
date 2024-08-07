6ade21983ea0943470ed003502d304a67dad5b8d
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-07 12:55:52 +0100

    fix:scrape recipe test

--- api/tests/controllers/fetchRecipeData.test.js
+++ api/tests/controllers/fetchRecipeData.test.js
@@ -3,24 +3,31 @@ const puppeteer = require("puppeteer");
 
 const RecipesController = require("../../controllers/recipes");
 
-const app = require("../../app");
+// const app = require("../../app");
 
 jest.mock("axios");
 jest.mock("puppeteer");
 
-require("../mongodb_helper");
+// require("../mongodb_helper");
 
-const secret = process.env.JWT_SECRET;
+// const secret = process.env.JWT_SECRET;
 
-let token;
+// let token;
 
 describe("RecipesController", () => {
   it("should fetch recipe data successfully", async () => {
     const mockAxiosResponse = {
-      data: "<html><body>Mock HTML content</body></html>",
+      data:
+        "<html><body><script type='application/ld+json'>{" +
+        '"@context": "http://schema.org",' +
+        '"@type": "Recipe",' +
+        '"recipeIngredient": ["1 cup flour", "2 eggs"],' +
+        '"recipeInstructions": ["Mix the ingredients.", "Bake for 20 minutes."]' +
+        "}</script></body></html>",
     };
 
     axios.get.mockResolvedValueOnce(mockAxiosResponse);
+    
     puppeteer.launch.mockResolvedValueOnce({
       newPage: jest.fn().mockResolvedValueOnce({
         goto: jest.fn().mockResolvedValueOnce(),
@@ -34,11 +41,16 @@ describe("RecipesController", () => {
     const req = { query: { url: "https://example.com" } };
     const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
-    await RecipesController.fetchRecipeData(req, res);
+    await RecipesController.scrapeRecipeFromWebsite(req, res);
 
     expect(axios.get).toHaveBeenCalledWith("https://example.com");
-    // console.log(res.status)
     expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        recipe_data: expect.any(Object),
+        token: expect.any(String),
+      })
+    );
   });
 
   it("should handle network failure", async () => {
@@ -47,7 +59,7 @@ describe("RecipesController", () => {
     const req = { query: { url: "https://example.com" } };
     const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
-    await RecipesController.fetchRecipeData(req, res);
+    await RecipesController.scrapeRecipeFromWebsite(req, res);
 
     expect(axios.get).toHaveBeenCalledWith("https://example.com");
     expect(res.status).toHaveBeenCalledWith(500);
