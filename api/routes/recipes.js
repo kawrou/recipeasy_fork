db72c981ccc2505ad456954bee8dd225442eebf5
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-07 11:32:52 +0100

    refactor:Changed names to match RESTful api conventions

--- api/routes/recipes.js
+++ api/routes/recipes.js
@@ -3,11 +3,12 @@ const RecipesController = require("../controllers/recipes.js")
 
 const router = express.Router();
 
-router.get("/scrape-recipe", RecipesController.fetchRecipeData);
-router.get('/', RecipesController.getAllRecipesByUserId); 
-router.get("/:recipe_id", RecipesController.getRecipeById);
+
+router.get('/', RecipesController.index); 
+router.get("/:recipe_id", RecipesController.show);
 router.post("/", RecipesController.create);
-router.patch("/:recipe_id", RecipesController.updateRecipe);
-router.patch("/favouritedByOwner/:recipe_id", RecipesController.isFavourite);
+router.patch("/:recipe_id", RecipesController.update);
+router.patch("/:recipe_id/favourite", RecipesController.markAsFavourite);
+router.get("/scrape", RecipesController.scrapeRecipeFromWebsite);
 
 module.exports = router;
