72817f6bf7bbcdb2a3cee0b0da83ba8114d8d28c
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-07 12:04:01 +0100

    refactor:fix empty test files and refactored instructions filtering

--- api/tests/utils/data/expected/recipe5.js
+++ api/tests/utils/data/expected/recipe5.js
@@ -1,1 +1,29 @@
-module.exports = {};
+module.exports = {
+  name: "Butter bean curry wraps",
+  image:
+    "https://images.immediate.co.uk/production/volatile/sites/30/2023/12/Butter-bean-curry-wraps-131c7b7.jpg?resize=768,574",
+  description:
+    "Use up the leftovers from our butter bean curry to make these filling wholemeal wraps. They're low in fat and calories, and rich in fibre",
+  tags: [
+    "Dinner",
+    "Lunch",
+    "BBC Good Food healthy diet plan",
+    "Butter bean curry",
+    "HDP",
+    "hdpwinter2024",
+    "healthy diet plan",
+    "Wraps",
+  ],
+  totalTime: 13,
+  recipeYield: 2,
+  recipeIngredient: [
+    "2 large wholemeal tortilla wraps",
+    "½ the butter bean curry (recipe below)",
+    "2 handfuls of mixed salad leaves",
+    "½ the raita (recipe below)",
+  ],
+  recipeInstructions: [
+    "Warm the wraps following pack instructions, or for a few seconds on each side over the gas flame of the hob to create a slight char.",
+    "Reheat leftover butter bean curry in a pan over a low heat until piping hot (if it’s quite wet, allow it to reduce slightly). Spread the curry over the centre of the wraps, then top with the salad and the raita. Roll up tightly and serve straightaway.",
+  ],
+};
