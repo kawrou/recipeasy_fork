8f3108f01f7c7adde5a936eae1309433cc25f50d
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-09 11:00:58 +0100

    fix:remove 'public' from svg import path

--- frontend/src/components/RecipePage/RecipeFields/RecipeYield.jsx
+++ frontend/src/components/RecipePage/RecipeFields/RecipeYield.jsx
@@ -4,7 +4,7 @@ import { QuantitySelector } from "../QuantitySelector";
 export const RecipeYield = ({ recipeYield, setRecipeYield, editMode }) => {
   return (
     <div className="flex items-center gap-1 pr-2 text-lg font-kanit font-bold text-secondary-500">
-      <img src="../../public/assets/servesIcon.svg" className="size-8" />
+      <img src="../../assets/servesIcon.svg" className="size-8" />
       <div className="align-middle pl-2 ">Serves</div>
       {editMode ? (
         <QuantitySelector quantity={recipeYield} setQuantity={setRecipeYield} />
