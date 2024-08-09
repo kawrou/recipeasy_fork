f1e5a6bb6ea7074f052c6dd4b3a41a717919f889
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-09 12:43:41 +0100

    feat:Add new routes to authentication

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -5,6 +5,10 @@ const AuthenticationController = require("../controllers/authentication");
 const tokenChecker = require("../middleware/tokenChecker");
 
 router.post("/", AuthenticationController.createToken);
+router.get("/refresh", AuthenticationController.refreshToken); 
+router.post("/logout", AuthenticationController.logout); 
+
+//TODO:I THINK UNECESSARY. Only used by RecipeScraper on frontend. But the route is already checked by a token checker!
 router.get("/", tokenChecker, AuthenticationController.checkToken);
 
 module.exports = router;
