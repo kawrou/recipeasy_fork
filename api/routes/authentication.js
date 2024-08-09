92205854bb938ebe320a112e681e0b017c362f3f
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-09 14:43:55 +0100

    ref:Remove if/else and use early return. Change error message

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const loginLimiter = require("../middleware/loginLimiter")
 const AuthenticationController = require("../controllers/authentication");
 const tokenChecker = require("../middleware/tokenChecker");
 
-router.post("/", AuthenticationController.createToken);
+router.post("/", loginLimiter, AuthenticationController.createToken);
 router.get("/refresh", AuthenticationController.refreshToken); 
-router.post("/logout", AuthenticationController.logout); 
+router.post("/logout", AuthenticationController.logOut); 
 
 //TODO:I THINK UNECESSARY. Only used by RecipeScraper on frontend. But the route is already checked by a token checker!
 router.get("/", tokenChecker, AuthenticationController.checkToken);
