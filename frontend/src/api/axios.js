e8a431f4d3c68b7f8d6088ac0dada859694a6787
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-22 18:04:08 +0100

    ref:Add req/res interceptors to axios.js instead of hook

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { authStore } from "./authStore";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export default axios.create({
+export const axiosPublic = axios.create({
   baseURL: BACKEND_URL || "https://localhost:3000",
 });
 
@@ -10,3 +12,47 @@ export const axiosPrivate = axios.create({
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
+
+axiosPublic.interceptors.request.use((config) => {
+  return config;
+});
+
+axiosPrivate.interceptors.request.use(
+  (config) => {
+    const accessToken = authStore.getAccessToken();
+    if (!config.headers["Authorization"]) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+axiosPrivate.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const prevRequest = error?.config;
+
+    if (error?.response.status === 401 && !prevRequest?.sent) {
+      prevRequest.sent = true;
+      const newAccessToken = await refresh();
+      prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+      return axiosPrivate(prevRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+const refresh = async () => {
+  const response = await axiosPublic.post("/tokens/refresh", {
+    withCredentials: true,
+  });
+  console.log("prev", JSON.stringify(authStore.getAccessToken));
+  console.log("refresh", response.data.token);
+  authStore.setAccessToken(response.data.token);
+  return response.data.token;
+};
