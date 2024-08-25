81d07e96a6a788e1658e123049eb8d26f5ab0dd8
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-25 10:17:09 +0100

    fix:useFetchRecipe tess

--- frontend/tests/hooks/useFetchRecipe.test.js
+++ frontend/tests/hooks/useFetchRecipe.test.js
@@ -1,63 +1,59 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import { useFetchRecipes } from "../../src/hooks/useFetchRecipe";
 import { beforeEach, vi, describe, it, expect } from "vitest";
-import { getAllRecipes } from "../../src/services/recipes";
+import useAxiosPrivate from "../../src/hooks/useAxiosPrivate";
 
-vi.mock("../../src/services/recipes", () => {
-  const getAllRecipesMock = vi.fn();
-  return { getAllRecipes: getAllRecipesMock };
+vi.mock("../../src/hooks/useAxiosPrivate", () => {
+  const axiosPrivateMock = {
+    get: vi.fn(),
+  };
+
+  return {
+    default: () => axiosPrivateMock,
+  };
 });
 
 describe("useFetchRecipe hook:", () => {
   beforeEach(() => {
-    vi.resetAllMocks();
+    vi.clearAllMocks();
   });
+
   it("fetches recipes from DB when token is present", async () => {
-    const token = "test token";
-    const setTokenMock = vi.fn();
     const mockRecipes = [
       { _id: "12345", title: "Test Recipe 1", duration: "45" },
       { _id: "23456", title: "Test Recipe 2", duration: "35" },
     ];
 
-    getAllRecipes.mockResolvedValue({ recipes: mockRecipes, token: "newToken" });
-    const { result } = renderHook(() => useFetchRecipes(token, setTokenMock));
+    const axiosPrivateMock = useAxiosPrivate();
+    axiosPrivateMock.get.mockResolvedValue({ data: { recipes: mockRecipes } });
+    const { result } = renderHook(() => useFetchRecipes());
+
+    await act(async () => {
+      await result.current.fetchRecipes();
+    });
 
     await waitFor(() => {
+      expect(axiosPrivateMock.get).toHaveBeenCalledWith("/recipes");
       expect(result.current.loading).toBeFalsy();
       expect(result.current.error).toBeNull();
       expect(result.current.recipes).toEqual(mockRecipes);
-      expect(setTokenMock).toHaveBeenCalledWith("newToken");
-    });
-  });
-
-  it("doesn't fetch recipes from DB when token isn't present", async () => {
-    const token = null;
-    const setTokenMock = vi.fn();
-    const { result } = renderHook(() => useFetchRecipes(token, setTokenMock));
-
-    await waitFor(() => {
-      expect(result.current.loading).toBeFalsy();
-      expect(result.current.error).toBeNull();
-      expect(result.current.recipes).toEqual([]);
-      expect(getAllRecipes).not.toHaveBeenCalled();
-      expect(setTokenMock).not.toHaveBeenCalled();
     });
   });
 
   it("returns an error", async () => {
-    const token = "test token";
-    const setTokenMock = vi.fn();
+    const axiosPrivateMock = useAxiosPrivate();
+    axiosPrivateMock.get.mockRejectedValue(new Error("test error"));
+    const { result } = renderHook(() => useFetchRecipes());
 
-    getAllRecipes.mockRejectedValue(new Error("test error"));
-    const { result } = renderHook(() => useFetchRecipes(token, setTokenMock));
+    await act(async () => {
+      await result.current.fetchRecipes();
+    });
 
     await waitFor(() => {
       expect(result.current.loading).toBeFalsy();
-      expect(result.current.error).toEqual(new Error("test error"));
+      expect(result.current.error).toEqual(true);
       expect(result.current.recipes).toEqual([]);
-      expect(setTokenMock).not.toHaveBeenCalled();
-      expect(getAllRecipes).toHaveBeenCalled();
+      expect(axiosPrivateMock.get).toHaveBeenCalled();
     });
   });
 });
