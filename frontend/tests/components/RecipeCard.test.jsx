72767b5dda75f8d8755df9c08de78d8748082b84
kawrou <121672581+kawrou@users.noreply.github.com>
2024-09-04 13:55:03 +0100

    test:RecipeCard test case

--- frontend/tests/components/RecipeCard.test.jsx
+++ frontend/tests/components/RecipeCard.test.jsx
@@ -1,13 +1,47 @@
 import { render, screen } from "@testing-library/react";
-
+import { userEvent } from "@testing-library/user-event";
+import { vi, describe, test, expect } from "vitest";
 import RecipeCard from "../../src/components/Recipe/RecipeCard";
+import { MemoryRouter } from "react-router-dom";
+import useAxiosPrivate from "../../src/hooks/useAxiosPrivate";
 
-describe.skip("Post", () => {
-  test("displays the message as an article", () => {
-    const testPost = { _id: "123", message: "test message" };
-    render(<RecipeCard post={testPost} />);
+vi.mock("../../src/hooks/useAxiosPrivate", () => {
+  const mockAxiosPrviate = {
+    patch: vi.fn(),
+  };
 
-    const article = screen.getByRole("article");
-    expect(article.textContent).toBe("test message");
+  return {
+    default: () => mockAxiosPrviate,
+  };
+});
+
+const user = userEvent.setup();
+
+const mockRecipe = {
+  _id: "1234",
+  image: "some_url",
+  name: "testName",
+  totalTime: 10,
+  favouritedByOwner: false,
+};
+
+describe("Post", async () => {
+  const mockAxiosPrivate = useAxiosPrivate();
+
+  test("it renders the card", async () => {
+    mockAxiosPrivate.patch.mockResolvedValue();
+
+    render(
+      <MemoryRouter>
+        <RecipeCard recipe={mockRecipe} />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByLabelText("reg-heart-icon")).toBeVisible();
+
+    const favBtn = screen.getByRole("button");
+    await user.click(favBtn);
+
+    expect(screen.getByLabelText("heart-icon")).toBeVisible();
   });
 });
