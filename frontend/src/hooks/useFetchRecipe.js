2ab68e2fb49ad1b1c8ec061f0c41c4245ae1d0b3
kawrou <121672581+kawrou@users.noreply.github.com>
2024-09-04 16:41:38 +0100

    ref:fetchRecipes now accepts a url path to make it more flexible

--- frontend/src/hooks/useFetchRecipe.js
+++ frontend/src/hooks/useFetchRecipe.js
@@ -2,39 +2,42 @@ import { useState, useCallback } from "react";
 import useAxiosPrivate from "./useAxiosPrivate";
 
 export const useFetchRecipes = () => {
-  const [recipes, setRecipes] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const axiosPrivate = useAxiosPrivate();
 
-  const fetchRecipes = useCallback(async (path) => {
-    try {
-      const response = await axiosPrivate.get(path);
-      setRecipes(response.data.recipes);
-      setLoading(false);
-    } catch (err) {
-      setLoading(false);
-      if (!err?.response) {
+  const fetchRecipes = useCallback(
+    async (path) => {
+      try {
+        const response = await axiosPrivate.get(path);
+        setData(response.data.data);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        if (!err?.response) {
+          setError({
+            type: "no-server-response",
+            message:
+              "No Server Response. Please check your internet connection or try again later.",
+          });
+          return;
+        }
+        if (err?.response.status === 401 || err?.response.status === 403) {
+          setError({
+            type: "auth-error",
+            message: "Unauthorized access. Please log in again.",
+          });
+          return;
+        }
         setError({
-          type: "no-server-response",
-          message:
-            "No Server Response. Please check your internet connection or try again later.",
+          type: "unexpected-error",
+          message: "An unexpected error occurred.",
         });
-        return;
       }
-      if (err?.response.status === 401 || err?.response.status === 403) {
-        setError({
-          type: "auth-error",
-          message: "Unauthorized access. Please log in again.",
-        });
-        return;
-      }
-      setError({
-        type: "unexpected-error",
-        message: "An unexpected error occurred.",
-      });
-    }
-  }, []);
+    },
+    [axiosPrivate],
+  );
 
-  return { recipes, loading, error, fetchRecipes };
+  return { data, loading, error, fetchRecipes };
 };
