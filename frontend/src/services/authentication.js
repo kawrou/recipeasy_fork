4569886fa28cc7dc437378de285236f2346f53e2
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-20 12:16:37 +0100

    doc:JSDoc documentatin for logIn and promiseHandler

--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -5,12 +5,27 @@ import { promiseHandler } from "./promiseHandler";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const LOGIN_URL = "/tokens";
 
+/**
+ * @typedef {Object} PromiseResponse
+ * @property {boolean} success - Indicates if the operation was successful.
+ * @property {Object} [response] - The data returned if the operation was successful.
+ * @property {Object} [error] - An object containing error details if the operation failed.
+ * @property {string} error.message - The error message associated with the error.
+ */
+
+/**
+ * @description - Takes a username and password param and makes a fetch request.
+ * @async
+ * @param {string} username - The username of the user.
+ * @param {string} password - The password of the user.
+ * @returns {Promise<PromiseResponse>} A promise that resolves to an object indicating the success status and user authentication or error information.
+ */
 export const logIn = async (username, password) => {
-  // if (!username || !password) {
-  //   throw new Error("Username and password are required.");
-  // }
   if (!username || !password) {
-    return { success: false, err: "Username and password are required." };
+    return {
+      success: false,
+      error: { message: "Username and password are required." },
+    };
   }
   const data = { username, password };
 
