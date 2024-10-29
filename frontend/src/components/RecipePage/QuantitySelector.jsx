ba3debbfc426b1a5dc5c809cef33075bd5cf85cd
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-29 11:21:44 +0000

    feat:Recipe yield red outline for validaiton UI

--- frontend/src/components/RecipePage/QuantitySelector.jsx
+++ frontend/src/components/RecipePage/QuantitySelector.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
-export const QuantitySelector = ({ quantity, setQuantity }) => {
+export const QuantitySelector = ({
+  quantity,
+  setQuantity,
+  error,
+  setErrors,
+}) => {
+
   const handleIncrement = () => {
     setQuantity(parseInt(quantity) + 1);
   };
@@ -10,15 +16,19 @@ export const QuantitySelector = ({ quantity, setQuantity }) => {
       setQuantity(parseInt(quantity) - 1);
     }
   };
-
+  // ${error && !quantity ? "border border-red-500 rounded-md" : ""}
   return (
     <>
-      <form className="max-w-xs">
+      <form className="max-w-xs ">
+        {/* <form
+          className={`max-w-xs ${error && !quantity ? "border-2 rounded border-red-500" : ""}`}
+        > */}
         <div className="relative flex items-center max-w-[8rem]">
           <button
             type="button"
             onClick={handleDecrement}
-            className="z-40 bg-blue-100 hover:bg-blue-200 border border-blue-300 rounded-s-lg p-3 h-11 focus:ring-blue-100 focus:ring-2 focus:outline-none"
+            // className="z-40 bg-blue-100 hover:bg-blue-200 border border-blue-300 rounded-s-lg p-3 h-11 focus:ring-blue-100 focus:ring-2 focus:outline-none"
+            className={`z-40 hover:bg-blue-200 border rounded-s-lg p-3 h-11  ${error && !quantity ? "border-red-500 bg-red-100" : "border-blue-300  bg-blue-100"}`}
           >
             <svg
               className="w-3 h-3 text-gray-900"
@@ -39,7 +49,8 @@ export const QuantitySelector = ({ quantity, setQuantity }) => {
           <input
             type="number"
             value={quantity}
-            className="z-0 font-kanit font-bold bg-blue-50 border border-x-0 border-blue-300 h-11 text-center text-secondary-500 text-md block w-full py-2.5 [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+            // className="z-0 font-kanit font-bold bg-blue-50 border border-x-0 border-blue-300 h-11 text-center text-secondary-500 text-md block w-full py-2.5 [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+            className={`z-0 font-kanit font-bold border border-x-0 ${error && !quantity ? "bg-red-50 border-red-500" : "bg-blue-50 border-blue-300"}  h-11 text-center text-secondary-500 text-md block w-full py-2.5 [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none`}
             placeholder="0"
             onChange={(e) => setQuantity(e.target.value)}
             required
@@ -47,7 +58,8 @@ export const QuantitySelector = ({ quantity, setQuantity }) => {
           <button
             type="button"
             onClick={handleIncrement}
-            className="z-40 bg-blue-100 hover:bg-blue-200 border border-blue-300 rounded-e-lg p-3 h-11 focus:ring-blue-100 focus:ring-2 focus:outline-none"
+            // className="z-40 bg-blue-100 hover:bg-blue-200 border border-blue-300 rounded-e-lg p-3 h-11 focus:ring-blue-100 focus:ring-2 focus:outline-none"
+            className={`z-40 hover:bg-blue-200 border rounded-e-lg p-3 h-11  ${error && !quantity ? "border-red-500 bg-red-100" : "border-blue-300  bg-blue-100"}`}
           >
             <svg
               className="w-3 h-3 text-blue-900 "
