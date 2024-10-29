de13e3d94866dca8f826f548857206b24154f4d1
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-29 11:59:57 +0000

    ref:ingredient field input handler

--- frontend/src/components/RecipePage/RecipeFields/IngredientList.jsx
+++ frontend/src/components/RecipePage/RecipeFields/IngredientList.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { FaPlus, FaTimes } from "react-icons/fa"; // Import icons from FontAwesome
 
-export const IngredientList = ({ recipeIngredients, setRecipeIngredients, editMode }) => {
+export const IngredientList = ({
+  recipeIngredients,
+  setRecipeIngredients,
+  editMode,
+  error,
+  setErrors,
+}) => {
   const [newIngredient, setNewIngredient] = useState("");
 
   const handleAddIngredient = () => {
@@ -23,22 +29,26 @@ export const IngredientList = ({ recipeIngredients, setRecipeIngredients, editMo
     setRecipeIngredients(updatedIngredients);
   };
 
+  const handleInput = (e, index) => {
+    const updatedIngredients = [...recipeIngredients];
+    updatedIngredients[index] = e.target.value;
+    setRecipeIngredients(updatedIngredients);
+  };
+
   return (
     <div className="flex w-1/2 flex-col pt-16 p-20 gap-7 rounded-3xl bg-white shadow-md ml-5 mr-2.5 mb-20 h-fit">
-      <div className="font-kanit font-extrabold text-primary-500 text-6xl text-left">Ingredients</div>
+      <div className="font-kanit font-extrabold text-primary-500 text-6xl text-left">
+        Ingredients
+      </div>
       <div className="flex flex-col">
         <div className="flex flex-col divide-y-2 divide-tertiary-500 font-poppins font-extralight text-gray-600">
-          {recipeIngredients.map((ingredient, index) => (
+          {recipeIngredients.map((ingredient, index) =>
             editMode ? (
               <div key={index} className="flex items-center py-4">
                 <input
                   className="resize-none overflow-hidden placeholder:text-wrap w-full p-2.5 focus:outline-none text-md rounded-xl border border-blue-200 "
                   value={ingredient}
-                  onChange={(e) => {
-                    const updatedIngredients = [...recipeIngredients];
-                    updatedIngredients[index] = e.target.value;
-                    setRecipeIngredients(updatedIngredients);
-                  }}
+                  onChange={(e) => handleInput(e, index)}
                   placeholder="Enter your ingredient..."
                 />
                 <button
@@ -50,9 +60,11 @@ export const IngredientList = ({ recipeIngredients, setRecipeIngredients, editMo
                 </button>
               </div>
             ) : (
-              <div key={index} className="text-left text-md py-2.5">{ingredient}</div>
-            )
-          ))}
+              <div key={index} className="text-left text-md py-2.5">
+                {ingredient}
+              </div>
+            ),
+          )}
         </div>
 
         {/* Input for new ingredient */}
@@ -64,13 +76,13 @@ export const IngredientList = ({ recipeIngredients, setRecipeIngredients, editMo
               className="px-2 py-1 rounded text-white"
               onClick={handleAddIngredient}
             >
-              <FaPlus className="text-secondary-500" /> {/* Change color to gray */}
+              <FaPlus className="text-secondary-500" />{" "}
+              {/* Change color to gray */}
             </button>
             <div className="w-1/2 border border-tertiary-500" />{" "}
             {/* Horizontal divider */}
           </div>
         )}
-
       </div>
     </div>
   );
