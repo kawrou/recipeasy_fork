9286256f740a9d5e9866d6d6ba273df341729b2f
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-30 17:07:18 +0000

    style:Toast notificaiton

--- frontend/src/components/Icons/ToastIcons.jsx
+++ frontend/src/components/Icons/ToastIcons.jsx
@@ -5,14 +5,14 @@ import {
   FaTimesCircle,
 } from "react-icons/fa";
 
-const CloseIcon = () => <VscClose />;
+const CloseIcon = () => <VscClose className="text-md" />;
 
-const SuccessIcon = () => <FaCheckCircle className="text-green-500 text-lg" />;
+const SuccessIcon = () => <FaCheckCircle className="text-green-500 text-2xl" />;
 
-const FailureIcon = () => <FaTimesCircle className="text-red-500 text-lg" />;
+const FailureIcon = () => <FaTimesCircle className="text-red-500 text-2xl" />;
 
 const WarningIcon = () => (
-  <FaExclamationCircle className="text-blue-600 text-lg" />
+  <FaExclamationCircle className="text-blue-600 text-2xl" />
 );
 
 export { CloseIcon, SuccessIcon, FailureIcon, WarningIcon };
