7fa7edae341b5fcf3cfa104d54181fb4d8a7775d
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-30 17:07:18 +0000

    style:Toast notificaiton

--- frontend/src/components/Toast/Toast.jsx
+++ frontend/src/components/Toast/Toast.jsx
@@ -26,20 +26,30 @@ const Toast = ({ message, type, duration = 5000 }) => {
 
   if (!isVisible) return null;
 
+  const handleCloseToast = () => {
+    setIsVisible(false);
+  };
+
   return (
     // Wrapper
     <div
-      className="fixed bottom-10 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black px-8 py-4 rounded-xl shadow-lg"
+      className={`fixed bottom-10 left-1/2 transform -translate-x-1/2
+         bg-gray-100 text-black px-8 py-4 rounded-xl shadow-lg max-w-lg 
+        transition-all duration-500 ease-in-out ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"}`}
       role="alert"
-      aria-label={`toast-${type}`}
+      aria-label={`notification-${type}`}
     >
       {/* Icon + Message */}
-      <div className="inline-flex">
+      <div className="inline-flex items-center space-x-2">
         {toastIcon && <div className="">{toastIcon}</div>}
         <p>{message}</p>
       </div>
       {/* Close toast button */}
-      <button className="">
+      <button
+        className="absolute top-2 right-2 ml-4"
+        aria-label="close-notification"
+        onClick={handleCloseToast}
+      >
         <span className="icon">
           <CloseIcon />
         </span>
