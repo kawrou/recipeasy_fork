08bec051374df67484f91ebe24e689eb3afd3f79
kawrou <121672581+kawrou@users.noreply.github.com>
2024-11-01 18:17:04 +0000

    feat:Add alt text prop to FigureImage component

--- frontend/src/components/HomePageFigureImage.jsx
+++ frontend/src/components/HomePageFigureImage.jsx
@@ -1,10 +1,10 @@
-const FigureImage = ({ image, title, text }) => {
+const FigureImage = ({ image, alt, title, text }) => {
   return (
     <figure className="">
       <img
         src={image}
         className="lg:w-2/3 mx-auto rounded-xl shadow-xl"
-        alt="Introducing how to import a recipe with url"
+        alt={alt}
         loading="lazy"
       />
       <figcaption className="bg-white m-7 p-4 rounded-xl shadow-xl lg:w-1/3 lg:mx-auto">
