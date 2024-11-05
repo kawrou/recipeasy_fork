d2d195dc34371574fa4ec47b49bbd9584d04ea8a
kawrou <121672581+kawrou@users.noreply.github.com>
2024-11-05 13:20:04 +0000

    ref:Tidy up and improve semantic html and css classes in RecipeCard

--- frontend/src/components/Recipe/RecipeCard.jsx
+++ frontend/src/components/Recipe/RecipeCard.jsx
@@ -3,42 +3,46 @@ import { FavouriteButton } from "../../components/RecipePage/FavouriteButton";
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <article className="w-full p-4">
-      <section className="bg-white rounded-3xl overflow-hidden flex flex-col gap-4 items-center shadow-md p-5">
-        <Link
-          to={`/recipes/${recipe._id}`}
-          className="w-full flex flex-col items-center"
-        >
-          <figure className="w-full">
-            <div
-              className="bg-cover bg-center h-48 w-full rounded-2xl"
-              style={{ backgroundImage: `url(${recipe.image})` }}
-            ></div>
-            <figcaption className="w-full text-left mf-4">
-              <h2 className="font-kanit text-2xl font-bold text-primary-500 mb-3">
-                {recipe.name}
-              </h2>
-            </figcaption>
-          </figure>
-        </Link>
-        <div className="flex items-center justify-between w-full mt-4">
-          <div className="flex items-center font-kanit font-bold gap-0.5 text-secondary-500">
-            <img
-              className="size-8 mr-2"
-              src="../../assets/timeTakenIcon.svg"
-              alt="Timer Image"
-            />
-            <p>{recipe.totalTime}</p>
-            <p> mins</p>
-          </div>
-
-          <FavouriteButton
-            recipeId={recipe._id}
-            favourited={recipe.favouritedByOwner}
-            size={30}
+    <article className=" overflow-hidden bg-white rounded-2xl flex flex-col justify-between gap-4 shadow-lg">
+      <Link to={`/recipes/${recipe._id}`} className="">
+        <figure className="">
+          {recipe.image ? (
+            <div className="overflow-hidden h-40">
+              <img
+                src={recipe.image}
+                alt={`${recipe.name} photo`}
+                loading="lazy"
+                className="object-cover bg-center h-full w-full transition-transform duration-300 ease-in-out transform hover:scale-105"
+              ></img>
+            </div>
+          ) : (
+            <div className="h-40 w-full bg-gray-200"></div>
+          )}
+          <figcaption className="text-left px-2">
+            <h3 className="font-kanit lg:text-lg text-orange-800 mb-3 hover:text-orange-200">
+              {recipe.name}
+            </h3>
+          </figcaption>
+        </figure>
+      </Link>
+      <div className="flex items-center justify-between px-2 mb-4">
+        <div className="flex items-center font-kanit text-gray-800">
+          <img
+            className="size-8 mr-2"
+            src="../../assets/timeTakenIcon.svg"
+            alt="Timer Image"
           />
+          <time dateTime={`PT${recipe.totalTime}M`} className="text-sm">
+            {recipe.totalTime} mins
+          </time>
         </div>
-      </section>
+
+        <FavouriteButton
+          recipeId={recipe._id}
+          favourited={recipe.favouritedByOwner}
+          size={30}
+        />
+      </div>
     </article>
   );
 };
